c2d48f694a467a91ca3a9b260e423bf07e1f0b1e
dev_kong <ahdjd5@gmail.com>
2022-05-23 16:24:02 +0900

    [user]feat: edit alias

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,4 +1,31 @@
 import { Request, Response } from 'express';
 import { Users } from '../model/user';
+import { Success, Failure } from '../types/response';
 
-export const editAlias = (req: Request, res: Response) => {};
+export const editAlias = async (req: Request, res: Response) => {
+  try {
+    const { u_idx, u_alias } = req.body;
+    const result = await Users.update({ u_alias }, { where: { u_idx } });
+    const data = (await Users.findOne({
+      where: { u_idx: result[0] },
+    })) as Users;
+    const response: Success<Users> = {
+      result: 'success',
+      data,
+    };
+  } catch (e) {
+    console.log(e);
+    let msg = '';
+    if (typeof e === 'string') {
+      msg = e;
+    } else if (e instanceof Error) {
+      msg = e.message;
+    }
+    const response: Failure<string> = {
+      result: 'fail',
+      error: msg,
+    };
+
+    res.send(response);
+  }
+};
