7fa38e5ec14bcd4e60567749dedde4e4ece61279
dev_kong <ahdjd5@gmail.com>
2022-05-26 17:34:20 +0900

    [memory]fix: create response data change to created memory

--- src/controller/memory.controller.ts
+++ src/controller/memory.controller.ts
@@ -16,12 +16,31 @@ export const create = async (req: Request, res: Response) => {
       .forEach(async (v) => {
         await MemoryImg.create({ m_idx: result.m_idx, img: v });
       });
+    const create = await MemoryMusic.create({
+      m_idx: result.m_idx,
+      link: music,
+    });
+    const data = (await Memory.findOne({
+      where: { m_idx: create.m_idx },
+      include: [
+        {
+          model: Users,
+          attributes: ['u_alias'],
+        },
+        {
+          model: MemoryMusic,
+          attributes: ['link'],
+        },
+        {
+          model: MemoryImg,
+          attributes: ['img'],
+        },
+      ],
+    })) as Memory;
 
-    await MemoryMusic.create({ m_idx: result.m_idx, link: music });
-
-    const response: Success<null> = {
+    const response: Success<Memory> = {
       result: 'success',
-      data: null,
+      data,
     };
     res.send(response);
   } catch (e) {
