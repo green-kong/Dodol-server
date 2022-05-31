c46e48a7bfb530afb73761981dc2d3c036dfbc63
jihyun kim <jihyunivy@gmail.com>
2022-05-31 11:17:37 +0900

    [location] fix: wrong word

--- src/controller/location.controller.ts
+++ src/controller/location.controller.ts
@@ -5,11 +5,11 @@ import axios from 'axios';
 
 
 export const list = async (req: Request, res: Response) => {
-  const { options } = req.body
+  const options = req.body
   console.log(options)
   let locationList;
   try {
-    const response = await axios.get(`https://dapi.kakao.com/v2/local/search/address.json?analyze_type=similar&page=1&size=10&query=${opitons.location}}`, options.headers)
+    const response = await axios.get(`https://dapi.kakao.com/v2/local/search/address.json?analyze_type=similar&page=1&size=10&query=${options.location}}`, options.headers)
     locationList = response.data
   } catch (e) {
     console.log(e)
